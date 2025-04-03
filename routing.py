08d60447f115f9faa2539a081f044ff92a787549
Lucius Scala <luciusscala@Luciuss-MacBook-Pro.local>
2025-04-02 20:43:28 -0700

    got all routes

--- routing.py
+++ routing.py
@@ -3,8 +3,12 @@ import sqlite3
 from bs4 import BeautifulSoup
 
 
+#connect to database and establish cursor
+conn = sqlite3.connect('database.db')
+cursor = conn.cursor()
+
 for i in range(1, 24):
-    url = 'https://www.ncaa.com/schools-index'+i
+    url = 'https://www.ncaa.com/schools-index/'+str(i)
     response = requests.get(url)
 
     soup = BeautifulSoup(response.text, 'html.parser')
@@ -13,17 +17,13 @@ for i in range(1, 24):
     table = soup.find('table')
     rows = table.find_all('tr')[1:]
 
-    #connect to database and establish cursor
-    conn = sqlite3.connect('database.db')
-    cursor = conn.cursor()
-
     for i in range(0, len(rows)):
         name = rows[i].find_all('td')[2].text.strip()
         link = rows[i].find_all('a')[0].get('href')
         cursor.execute('UPDATE schools SET route = ? WHERE name = ?', (link, name))
 
-    # Commit changes to the database
-    conn.commit()
+# Commit changes to the database
+conn.commit()
 
 # Close the cursor and the connection
 cursor.close()
