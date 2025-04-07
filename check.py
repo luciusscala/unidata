b7fc654d50f2a333fe40e4f32f8d7970e8051a37
Lucius Scala <luciusscala@Luciuss-MacBook-Pro.local>
2025-04-07 14:25:32 -0700

    started frontent

--- check.py
+++ check.py
@@ -22,15 +22,20 @@ def verify(email: str, name: str):
         raise HTTPException(status_code=404, detail='School not found')
     
     website = result[0]
+    #TODO more fullproof method of getting webiste (not all are .com)
+    #TODO add other sports not just mens soccer
     roster = website + '/sports/mens-soccer/roster'
 
-    #get the roster names
+    #get the roster
+    #TODO fix check for wrong page loaded
     try:
         page = requests.get(roster)
         soup = BeautifulSoup(page.text, 'html.parser')
     except Exception:
         raise HTTPException(status_code=500, detail='Failed to fetch roster')
     
+
+    #TODO find better method
     if name.lower() in soup.get_text().lower():
         return {"status": "valid"}
     else:
